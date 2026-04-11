5d1c6d25a1cbff175f51ff37594b1faa79587f5e
Olaf Tomczak <olaf.tomczak@gmail.com>
2026-04-11 22:41:02 +0200

    Fixing pom.xml generation

--- modules/kex/build.gradle.kts
+++ modules/kex/build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
     id("library-convention")
 }
-project.description = "Kotlin exception utilities - a lightweight library providing extension functions for better exception handling in Kotlin"
+project.description =
+    "Kotlin exception utilities - a lightweight library providing extension functions for better exception handling in Kotlin"
